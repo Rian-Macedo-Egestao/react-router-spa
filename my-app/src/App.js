7a5b9f7e9643717559df48ab9dfb78927284af42
Rian Macedo <rian.macedo@e-gestao.com.br>
2021-10-14 11:05:42 -0300

    instalado dependencias do react

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./paginas/Home";
+import About from "./paginas/About";
+
+function App() {
+  return (
+    <>
+    <Home/>
+    <About/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/paginas/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About(){
+    return (
+        <div>
+            <h1>
+                Informações sobre o Doguitto
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec feugiat urna, eu volutpat risus. Vestibulum euismod eleifend arcu ultricies rutrum. Suspendisse potenti. Nulla facilisi. Mauris malesuada, enim at condimentum finibus, odio sapien aliquam leo, vitae efficitur orci velit nec elit. Quisque euismod lorem quis orci fermentum, blandit placerat metus suscipit. Sed nec ante et erat elementum malesuada cursus id erat. Pellentesque auctor sollicitudin sem, non interdum eros congue efficitur.
+            </p>
+        </div>
+        
+    );
+}
+
+export default About;

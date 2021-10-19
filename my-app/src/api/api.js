668bb6990d97510085a342787f24d98c4dd24d2a
Rian Macedo <rian.macedo@e-gestao.com.br>
2021-10-19 14:47:48 -0300

    corrigido problema de nao encontrar os posts

--- my-app/src/api/api.js
+++ my-app/src/api/api.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
 
+
 export const api = axios.create({
-    baseURL:'http://localhost:5000'
+  baseURL: 'http://localhost:5000'
+})
 
-});
-
-export const Busca = async(url, setDado) => {
-    const resposta = await api.get(url);
-    setDado(resposta.data);
-
+export const busca = async(url, setDado) => { 
+  const reposta = await api.get(url)
+  setDado(reposta.data)
 }
--- my-app/src/components/ListaPost.jsx
+++ my-app/src/components/ListaPost.jsx
@@ -1,31 +1,33 @@
-import React from "react";
-import { useState ,  useEffect} from "react";
-import { Link } from "react-router-dom";
-import {Busca} from "../api/api";
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { busca } from '../api/api'
 
 
-const ListaPost = ({url}) => {
-    const [posts, setPosts] = useState([])
 
-    useEffect(() => {
-        Busca(url, setPosts)
-    }, [])
+const ListaPost = ( { url } ) => { 
 
-    return (
-        <section className="posts container">
+const [posts, setPosts] = useState([])
 
-            {posts.map((post, index) => {
-                <Link className={`cartao-post cartao-post--${post.categoria}`}> 
-                <article key={post.id}>
-                    <h3 className="cartao-post__titulo">{post.title}</h3>
-                    <p className="cartao-post__meta">{post.metadescription}</p>
+useEffect(() => {
+  busca(url, setPosts)
+}, [])
 
-                </article>
-                </Link>
-                
-                })
-            }
-        </section>
-    )
+  return(
+    <section className="posts container">
+      { 
+       posts.map((post)=> (
+         <Link className={`cartao-post cartao-post--${post.categoria}`} to={`/posts/${post.id}`}>
+           <article key={post.id}>
+              <h3 className="cartao-post__titulo">
+                {post.title}
+              </h3>
+              <p className="cartao-post__meta">{post.metadescription}</p>
+           </article> 
+         </Link>
+       ))
+      }
+    </section>
+  )
 }
+
 export default ListaPost
